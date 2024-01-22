b80ee78d627ae8548e589ba1fc3a4db91f3b6c96
OlgaOparyna <olka-rb@yandex.ru>
2024-01-22 12:17:16 +0300

    fix: update  04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
+fs.rmdir(path.resolve(__dirname, 'files-copy'), (err) => {
+  if (err) {
+    throw err;
+  }
+});
+
 fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
   if (err) {
     return console.error(err);
   }
   console.log('Directory created successfully!');
+});
+
+fs.promises.readDir(path.join(__dirname, 'files')).then((files) => {
+  files.forEach((file) => {
+    const filePath = path.join(__dirname, 'files', file);
+    fs.promises.copyFile(filePath, path.join(__dirname, 'files-copy', file));
+  });
 });
